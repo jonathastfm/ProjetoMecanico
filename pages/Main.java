8e926dc3377be84510ab2621964465a4c7d11258
b10-Lenovo <jonathastfm@gmail.com>
2024-12-05 10:39:27 -0300

    Implement CRUD interfaces for Carros, Servicos, and Funcionarios with GUI in separate classes

--- pages/Main.java
+++ pages/Main.java
@@ -1,5 +1,6 @@
 package ProjetoMecanico;
 
+import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,6 +8,7 @@ import java.awt.event.ActionListener;
 import ProjetoMecanico.CarrosCrud;
 import ProjetoMecanico.ServicosCrud;
 import ProjetoMecanico.FuncionariosCrud;
+
 
 public class Main {
     public static void main(String[] args) {
